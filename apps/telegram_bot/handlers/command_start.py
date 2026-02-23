999bf5a6e3fa5284cd9eac21d114eb698f2d21ea
pricheta <pricheta@inbox.ru>
2026-02-23 12:37:27 +0800

    open_debt_menu

--- apps/telegram_bot/handlers/command_start.py
+++ apps/telegram_bot/handlers/command_start.py
@@ -1,21 +1,32 @@
 from aiogram.types import (
     Message,
-    MenuButtonCommands,
-    ReplyKeyboardMarkup,
-    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery,
 )
 
-START_MESSAGE = 'Привет! Я - бот для автоматизации рутины. Выбери приложение'
+
+START_MESSAGE = 'Привет! Я - бот для автоматизации рутины. Выбери приложение:'
 
 DEBT_ACCOUNTER_BUTTON_TEXT = '📋 Долги'
+DEBT_ACCOUNTER_BUTTON = InlineKeyboardButton(
+    text=DEBT_ACCOUNTER_BUTTON_TEXT,
+    callback_data=DEBT_ACCOUNTER_BUTTON_TEXT,
+)
 
-start_keyboard = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text=DEBT_ACCOUNTER_BUTTON_TEXT)],
-    ],
-    resize_keyboard=True,
+start_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            DEBT_ACCOUNTER_BUTTON,
+        ],
+    ]
 )
 
 
 async def command_start_handler(message: Message) -> None:
     await message.answer(START_MESSAGE, reply_markup=start_keyboard)
+
+
+async def open_debt_menu(callback: CallbackQuery) -> None:
+    await callback.message.answer('Открываю учёт долгов...')  # type: ignore
+    await callback.answer()
--- apps/telegram_bot/main.py
+++ apps/telegram_bot/main.py
@@ -3,14 +3,17 @@ import logging
 import sys
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 
 from apps.telegram_bot.config import BotConfig
-from apps.telegram_bot.handlers.command_start import command_start_handler
-
+from apps.telegram_bot.handlers.command_start import (
+    command_start_handler,
+    DEBT_ACCOUNTER_BUTTON_TEXT,
+    open_debt_menu,
+)
 
 load_dotenv()
 config = BotConfig()  # type: ignore
@@ -18,6 +21,8 @@ config = BotConfig()  # type: ignore
 
 dp = Dispatcher()
 dp.message(CommandStart())(command_start_handler)
+
+dp.callback_query(F.data == DEBT_ACCOUNTER_BUTTON_TEXT)(open_debt_menu)
 
 
 async def main() -> None:
