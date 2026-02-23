2e10f3dd7b40f107c3685d747ce9ec7eac233b16
pricheta <pricheta@inbox.ru>
2026-02-23 12:46:43 +0800

    open_debt_menu

--- apps/telegram_bot/main.py
+++ apps/telegram_bot/main.py
@@ -13,7 +13,7 @@ from apps.telegram_bot.handlers.command_start import (
     command_start_handler,
     DEBT_ACCOUNTER_BUTTON_TEXT,
 )
-from apps.telegram_bot.handlers.debt_accounter.open_debt_menu import open_debt_menu
+from apps.telegram_bot.handlers.debt_accounter import open_debt_menu
 
 load_dotenv()
 config = BotConfig()  # type: ignore
